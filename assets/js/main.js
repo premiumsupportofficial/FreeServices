a5de7f029d4741b423bad6e05885d82ed4d14516
Anojan Kumara <152652524+premiumsupportofficial@users.noreply.github.com>
2024-05-11 14:29:52 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,6 +162,23 @@ function activateProduct(parameter) {
 
 parameters.forEach(activateProduct);
 
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function moveSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("carousel_slide");
+    if (n >= slides.length) {slideIndex = 0}
+    if (n < 0) {slideIndex = slides.length - 1}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
 
 var slideIndex = 1;
 showSlides(slideIndex);
